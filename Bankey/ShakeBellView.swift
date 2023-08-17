49ec14a3dfd8561a4311f9e9319d2181dabf8e3d
gabrielgarciapimentel <130492130+gabrielgarciapimentel@users.noreply.github.com>
2023-08-17 18:34:44 -0300

    FEAT: Adding the badge

--- Bankey/ShakeBellView.swift
+++ Bankey/ShakeBellView.swift
@@ -10,6 +10,10 @@ import UIKit
 class ShakeBellView: UIView {
     let imageView = UIImageView()
     
+    let buttonView = UIButton()
+    
+    let buttonHeight: CGFloat = 16
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
@@ -38,16 +42,31 @@ extension ShakeBellView {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         let image = UIImage(systemName: "bell.fill")!.withTintColor(.white, renderingMode: .alwaysOriginal)
         imageView.image = image
+        
+        buttonView.translatesAutoresizingMaskIntoConstraints = false
+        buttonView.backgroundColor = .systemRed
+        buttonView.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        buttonView.layer.cornerRadius = buttonHeight / 2
+        buttonView.setTitle("9", for: .normal)
+        buttonView.setTitleColor(.white, for: .normal)
     }
     
     func layout() {
         addSubview(imageView)
+        addSubview(buttonView)
         
         NSLayoutConstraint.activate([
             imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
             imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
             imageView.widthAnchor.constraint(equalToConstant: 24),
             imageView.heightAnchor.constraint(equalToConstant: 24)
+        ])
+        
+        NSLayoutConstraint.activate([
+            buttonView.topAnchor.constraint(equalTo: imageView.topAnchor),
+            buttonView.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: -9),
+            buttonView.widthAnchor.constraint(equalToConstant: 16),
+            buttonView.heightAnchor.constraint(equalToConstant: 16)
         ])
     }
 }
